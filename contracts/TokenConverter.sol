921e8bd2c95df4decfe2e51e4b3588ee0fe77a67
uivlis <vladsilviufarcas@gmail.com>
2018-10-28 12:16:25 +0200

    Value passed to BancorNetwork.convert() in case of Ether

--- contracts/TokenConverter.sol
+++ contracts/TokenConverter.sol
@@ -39,6 +39,7 @@ contract TokenConverter {
         IERC20Token[] memory path = new IERC20Token[](3);
         SmartToken myBit = SmartToken(0x5d60d8d7eF6d37E16EBABc324de3bE57f135e0BC); 
         uint amount = _amount;
+        uint value = 0;
         if (msg.value == 0){
             require(bancorNetwork.etherTokens(_token) == true, "Token not supported");
             token = SmartToken(_token);
@@ -47,11 +48,12 @@ contract TokenConverter {
         } else {
             token = EtherToken(0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315);
             amount = msg.value;
+            value = msg.value;
         }
         path[0] = token;
         path[1] = myBit;
         path[2] = myBit;
-        uint convertedValue = bancorNetwork.convert(
+        uint convertedValue = bancorNetwork.convert.value(value)(
             path,
             amount,
             _minimumReturn
